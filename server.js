9fa8ff4b23dce4fc7d7baf947ca0f8c5fe17b571
Kai Wells <support@kaiwells.me>
2018-01-20 13:01:01 -0800

    DAMN YOU CORS *shakes fist*

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const bp = require("body-parser");
 const app = express();
 const port = process.env.PORT || 8000;
 
+// CORS
+// https://stackoverflow.com/questions/11001817/allow-cors-rest-request-to-a-express-node-js-application-on-heroku
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+});
+
 app.use(bp.urlencoded({extended: false}));
 app.use(bp.json());
 
