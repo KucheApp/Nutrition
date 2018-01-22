10d3bd2ef6aec87756d948f113d706ffe8806eb8
Kai Wells <support@kaiwells.me>
2018-01-21 16:35:10 -0800

    Add search by id

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 
-function buildStmt(search_str) {
+function buildSearchStmt(search_str) {
   search_str = search_str.replace(/\'/g, "''");
   let metaphone_similarity = 20;
   let response_limit = 20;
@@ -11,33 +11,48 @@ function buildStmt(search_str) {
   ORDER BY levenshtein(metaphone, metaphone('${search_str}', ${metaphone_similarity}))
   LIMIT ${response_limit}
   ;
-  `
+  `;
+}
+
+function buildIdStmt(id) {
+  return `SELECT * FROM foods WHERE f_id = ${id};`;
 }
 
 function parseFoodRow(pg_row) {
-  let {name, calories, fat_mg, sodium_mg, carbs_mg, fiber_mg, sugars_mg, protein_mg} = pg_row;
+  let {f_id, name, calories, fat_mg, sodium_mg, carbs_mg, fiber_mg, sugars_mg, protein_mg} = pg_row;
   let fat_g = fat_mg * 0.001;
   let carbs_g = carbs_mg * 0.001;
   let fiber_g = fiber_mg * 0.001;
   let sugars_g = sugars_mg * 0.001;
   let protein_g = protein_mg * 0.001;
   let serving_size_g = 100;
-  return {name, serving_size_g, calories, fat_g, sodium_mg, carbs_g, fiber_g, sugars_g, protein_g};
+  return {f_id, name, serving_size_g, calories, fat_g, sodium_mg, carbs_g, fiber_g, sugars_g, protein_g};
+}
+
+function filterNaughtyKeywords(query) {
+  var naughtyKeywords = ["UPDATE", "DELETE", "DROP", "INSERT", "CREATE", "ALTER", ";"];
+  naughtyKeywords.forEach(kw => {
+    if (query.includes(kw)) {
+      return Promise.reject('probably sql injection')
+    }
+  });
+  return Promise.resolve(query);
 }
 
 module.exports = (connection_uri) => {
   const pool = new Pool({ connectionString: connection_uri });
   return {
     search: function(query) {
-      var naughtyKeywords = ["UPDATE", "DELETE", "DROP", "INSERT", "CREATE", "ALTER", ";"];
-      naughtyKeywords.forEach(kw => {
-        if (query.includes(kw)) {
-          return Promise.reject('probably sql injection')
-        }
-      });
-
-      let stmt = buildStmt(query);
-      return pool.query(stmt)
+      return filterNaughtyKeywords(query)
+      .then(query => buildSearchStmt(query))
+      .then(stmt => pool.query(stmt))
+      .then(result => result.rows)
+      .then(rows => rows.map(parseFoodRow))
+    },
+    withId: function(id) {
+      return filterNaughtyKeywords(id)
+      .then(query => buildIdStmt(query))
+      .then(stmt => pool.query(stmt))
       .then(result => result.rows)
       .then(rows => rows.map(parseFoodRow))
     }
--- search.js
+++ search.js
@@ -7,12 +7,21 @@ module.exports = (app) => {
   const db = require("./db")(connection_uri);
 
   router.get("/search", (req, res) => {
-    db.search(req.query.food)
-    .then(results => res.json(results))
-    .catch(err => {
-      console.log(err);
-      res.sendStatus(500);
-    })
+    if (req.query.id !== undefined) {
+      db.withId(req.query.id)
+      .then(results => res.json(results))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+      })
+    } else if (req.query.food !== undefined) {
+      db.search(req.query.food)
+      .then(results => res.json(results))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+      })
+    }
   })
 
   return router;
